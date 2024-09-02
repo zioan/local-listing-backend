16e9234c371e6c98e4da530668103d5a3076edad
zioan <zaharia.ioan@gmail.com>
2024-09-03 01:08:03 +0200

    Remove unsecured hardcoded SECRET_KEY from settings.py

--- locallisting/settings.py
+++ locallisting/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i@oxxumyy2tdq5x64d(0@-@anzt^^ol&ginusukk4i%xiz44^e"
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
