118412b429ffc303c971dd72939ad4c88663e5f8
zioan <zaharia.ioan@gmail.com>
2024-09-02 14:27:21 +0200

    Add user views and urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from .serializers import (
+    UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+)
 
-# Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": UserProfileSerializer(
+                user, context=self.get_serializer_context()
+            ).data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(
+            request,
+            email=serializer.validated_data['email'],
+            password=serializer.validated_data['password']
+        )
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": UserProfileSerializer(user).data,
+                "token": token.key
+            })
+        return Response(
+            {"error": "Invalid Credentials"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
