ac716d2bb5ec080acca093e7a0a5c791ed6e4959
zioan <zaharia.ioan@gmail.com>
2024-09-02 14:25:10 +0200

    Add missing dependencies

--- locallisting/settings.py
+++ locallisting/settings.py
@@ -44,7 +44,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "users",
 ]
+
+# DRF settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
