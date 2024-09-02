8a684e8a5e980dab1fe2ca24f78841aeb06efa14
zioan <zaharia.ioan@gmail.com>
2024-09-02 14:27:48 +0200

    Register user app to admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_active',
+                    'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('street', 'zip', 'city')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+         'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'username', 'password1', 'password2',
+                'is_active', 'is_staff', 'is_superuser'
+            )
+        }),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
