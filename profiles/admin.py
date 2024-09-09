07c4ff7db3b6447d69b41e1e4b3f543a02bf7270
zioan <zaharia.ioan@gmail.com>
2024-09-09 22:21:29 +0200

    Add profiles to admin dashboard

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'location', 'total_listings',
+                    'active_listings', 'rating')
+    search_fields = ('user__username', 'location')
+
+    def username(self, obj):
+        return obj.user.username
