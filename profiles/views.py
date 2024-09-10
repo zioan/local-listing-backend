4bed6aa026a843246126c4ddb3c51b81bc4c8d10
zioan <zaharia.ioan@gmail.com>
2024-09-10 12:32:51 +0200

    Fix user listings retrieval and counts for public profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, permissions
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer, PrivateProfileSerializer
 from listings.models import Listing
@@ -19,10 +21,24 @@ class PublicProfileView(generics.RetrieveAPIView):
     lookup_field = 'user__username'
     lookup_url_kwarg = 'username'
 
+    def retrieve(self, request, *args, **kwargs):
+        username = self.kwargs.get('username')
+        profile = get_object_or_404(Profile, user__username=username)
+        profile.update_listing_counts()
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data)
+
 
 class UserListingsView(generics.ListAPIView):
     serializer_class = ListingSerializer
 
     def get_queryset(self):
         username = self.kwargs['username']
-        return Listing.objects.filter(user__username=username, is_active=True)
+        queryset = Listing.objects.filter(user__username=username)
+        active_queryset = queryset.filter(is_active=True)
+        return active_queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
